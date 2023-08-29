e235c1bf690f3eca3e4a8b9e30e2a59058f1103f
YassineIDR <yassineidrissi2005@gmail.com>
2023-08-29 22:31:13 +0100

    some improvements

--- client/src/components/AddPost.vue
+++ client/src/components/AddPost.vue
@@ -17,7 +17,7 @@
         <div class="flex flex-row items-center justify-between m-2" v-if="onClickCaption">
             <!-- file choices -->
             <div class="flex flex-row gap-3 justify-start">
-                <input type="file" class="file-input file-input-ghost w-full max-w-xs"/>
+                <input type="file" class="file-input file-input-ghost w-full max-w-xs" @change="handleImage"/>
             </div>
             <!-- Post button -->
                 <button class="btn btn-circle btn-ghost btn-outline " :disabled="!onInputCaption"><i class="fa-solid fa-plus text-xl font-thin"></i></button>
@@ -28,9 +28,15 @@
 import { ref } from 'vue';
 const onClickCaption = ref(false)
 const onInputCaption = ref(false)
-
+const image = {
+    data: '',
+}
 const handleInput = (event) =>{
     onInputCaption.value = event.target.value.trim() !== ""
+}
+
+const handleImage = (event)=>{
+    console.log(event)
 }
 
 
