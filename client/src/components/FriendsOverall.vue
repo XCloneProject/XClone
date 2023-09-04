6f2bb7fa4d088f57c85e36fa47fe83c1b2703d8e
YassineIDR <yassineidrissi2005@gmail.com>
2023-09-04 11:56:29 +0100

    more ui imporvements

--- client/src/components/FriendsOverall.vue
+++ client/src/components/FriendsOverall.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col " v-if="showList">
-    <div class="flex flex-row justify-between text-2xl py-8 p-12 cursor-pointer">
-      <h1 class="  font-medium hover:border-b" :class="{'border-b':showFriendList}" @click="toggleFriends">Friend List</h1>
-      <h1 class=" font-medium hover:border-b" :class="{'border-b':showSuggestions}" @click="toggleSuggestions">Suggestions</h1>
+  <div class="flex flex-col" v-if="showList">
+      <div class="tabs mt-16 justify-center">
+      <a class="tab tab-lifted text-2xl" :class="{ 'tab-active': showFriendList }" @click="toggleFriends">Friends</a>
+      <a class="tab tab-lifted text-2xl" :class="{ 'tab-active': showSuggestions }" @click="toggleSuggestions">Requests</a>
+      <hr class="w-full my-1">
     </div>
-    <hr class="w-1/2 mx-auto">
-    <friendList v-if="showFriendList"/>
-    <SuggestedFriends v-if="showSuggestions"/>
+    <friendList v-if="showFriendList" />
+    <SuggestedFriends v-if="showSuggestions" />
   </div>
 </template>
 
@@ -25,15 +25,15 @@ defineProps({
 const showFriendList = ref(true)
 const showSuggestions = ref(false)
 
-const toggleFriends = ()=>{
+const toggleFriends = () => {
   showFriendList.value = true
   showSuggestions.value = false
 }
 
-const toggleSuggestions = ()=>{
+const toggleSuggestions = () => {
   showSuggestions.value = true
   showFriendList.value = false
-  
+
 }
 
 </script>
--- client/src/components/PostComp.vue
+++ client/src/components/PostComp.vue
@@ -3,14 +3,25 @@
     <hr class="w-full">
     <!-- Profile -->
     <div class=" flex flex-col gap-4 w-full my-2 p-3">
-      <div class="flex flex-row gap-4 items-center">
-        <div class="avatar  cursor-pointer">
-          <div class="w-14 rounded-full">
-            <img src="../assets/Profile.jpg"/>
+      <div class="flex flex-rowitems-center justify-between">
+        <div class="flex flex-row gap-4">
+          <div class="avatar  cursor-pointer">
+            <div class="w-14 rounded-full">
+              <img src="../assets/Profile.jpg" />
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <p class="text-xl">Yassine Idr</p>
+            <p class="flex-1 text-sm text-slate-300 ">time</p>
           </div>
         </div>
-        <p class="text-xl">Yassine Idr</p> 
-        <p class="flex-1 text-right text-sm text-slate-300 ">time</p>
+        <div class="dropdown dropdown-bottom ">
+          <label tabindex="0"><i class="fa-solid fa-ellipsis cursor-pointer"></i></label>
+          <ul tabindex="0" class="dropdown-content z-[1] menu shadow bg-base-200 rounded-box w-56">
+            <li><a>Edit</a></li>
+            <li @click="toggleConfirmation"><a>Delete post</a></li>
+          </ul>
+        </div>
       </div>
       <!-- Caption -->
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus at exercitationem voluptas cum itaque voluptatum
@@ -20,17 +31,16 @@
         <img src="../assets/post-test.jpg" alt="" class="w-cover">
       </div>
       <!-- Reacts -->
-       <div class="flex flex-row justify-between cursor-pointer">
+      <div class="flex flex-row justify-between cursor-pointer">
         <div class="flex flex-row gap-7">
           <div @click="handleLike">
             <i class="fa-regular fa-heart text-xl" v-if="!isLiked"></i>
             <i class="fa-solid fa-heart text-xl text-red-600" v-if="isLiked"></i>
           </div>
-          <i class="fa-regular fa-comment text-xl"></i>
+          <i class="fa-regular fa-comment-dots text-xl"></i>
         </div>
         <i class="fa-solid fa-share text-xl"></i>
-       </div>
-
+      </div>
     </div>
   </div>
 </template>
@@ -40,14 +50,19 @@ import { ref } from 'vue';
 import axios from 'axios';
 
 const isLiked = ref(false)
+const showDeleteConfirmation = ref(false)
 const Posts = [{
   id: "",
   photo: null,
-  caption:""
+  caption: ""
 }]
 
-const handleLike = ()=>{
+const handleLike = () => {
   isLiked.value = !isLiked.value
+}
+
+const toggleConfirmation = ()=>{
+  showDeleteConfirmation.value = !showDeleteConfirmation.value;
 }
 </script>
 
